6318ec92b66aec4b123821207234a52bb6ab12c8
codebender <roch@paystack.com>
2022-04-19 17:28:59 +0200

    making repo public

--- dist/index.c36f364e.js
+++
@@ -1,7 +0,0 @@
-const send = ()=>{
-    const ref = document.getElementById('ref').value;
-    window.location = `https://pstk-shop.paystackintegrations.com/finalize?shop=nifty-gifts-za.myshopify.com&reference=${ref}`;
-    console.log('sending', ref);
-};
-
-//# sourceMappingURL=index.c36f364e.js.map
