35cb323336e880898b8085985543fbb7fa79180d
lukman-paystack <58217917+lukman-paystack@users.noreply.github.com>
2022-09-19 14:17:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const send = () => {
 
     const ref = document.getElementById('ref').value;
 
-    window.location = `https://pstk-shop.paystackintegrations.com/finalize?shop=nifty-gifts-za.myshopify.com&reference=${ref}`
+    window.location = `https://pstk-shop.paystackintegrations.com/callback?trxref=${ref}&reference=${ref}`
     console.log('sending', ref)
 }
